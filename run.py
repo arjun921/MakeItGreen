c7cadefb033a47047d82aaa27e074ecd25f85831
Arjun S <arjun921@gmail.com>
2017-10-24 17:59:11 +0530

    3CQDC3Q1J6JJ67W

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def rand_string():
 
 def make_changes():
 	fname = rand_string()
-	writing = open(fname,'w+')
+	writing = open(path+fname,'w+')
 	temp = random.randrange(1,125)
 	print(temp)
 	for x in range(temp):
@@ -21,8 +21,14 @@ def make_changes():
 	os.system("git add .")
 	os.system("git commit -m "+rand_string())
 
+link = input("Enter link to private repo for writing to remote")
+
+path = input("Enter path where you want to clone and make Misc Changes every time you run MakeItGreen")
+
+os.system("git clone "+link+" "+path)
+
 for x in range(s):
 	make_changes()
 
-os.system("git log")
+# os.system("git log")
 #print(s)
