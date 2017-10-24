3c37312ba91fa5133dab6fad7d23a2c00db67e9d
Arjun S <arjun921@gmail.com>
2017-10-24 23:44:19 +0530

    Released version 0.6.1

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 setup(
-     name='makeitgreen',    # This is the name of your PyPI-package.
-     version='0.2',                          # Update the version number for new releases
-     scripts=['mig']                  # The name of your scipt, and also the command you'll be using for calling it
+    name='makeitgreen',    # This is the name of your PyPI-package.
+    version='0.6.2',                          # Update the version number for new releases
+    scripts=['mig'],                  # The name of your scipt, and also the command you'll be using for calling it
+    description='Makes your Git Green every time you run this package'
 )
