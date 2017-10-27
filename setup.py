4d1a48a519841ff8c2c9aebdc20f1c32d46ec566
Arjun S <arjun921@gmail.com>
2017-10-27 18:35:03 +0530

    Update to v2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='makeitgreen',    # This is the name of your PyPI-package.
-    version='1.0',                          # Update the version number for new releases
+    version='2.0',                          # Update the version number for new releases
     scripts=['mig'],                  # The name of your scipt, and also the command you'll be using for calling it
     description='Makes your Git Green every time you run this package'
 )
